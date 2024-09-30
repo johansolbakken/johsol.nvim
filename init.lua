c535af55835143cc7b38dd3625095ed784f168b7
Johan Solbakken <johan.hvaler@gmail.com>
2024-09-30 11:40:41 +0200

    Color theme to catpuccino

--- init.lua
+++ init.lua
@@ -817,12 +817,26 @@ require('lazy').setup({
       -- Load the colorscheme here.
       -- Like many other themes, this one has different styles, and you could load
       -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-      vim.cmd.colorscheme 'tokyonight-night'
 
       -- You can configure highlights by doing something like:
       vim.cmd.hi 'Comment gui=none'
     end,
   },
+  {
+    'rebelot/kanagawa.nvim',
+    priority = 1000, -- Make sure to load this before all the other start plugins.
+    init = function()
+      -- Load the colorscheme here.
+
+      -- You can configure highlights by doing something like:
+      vim.cmd.hi 'Comment gui=none'
+    end,
+
+  },
+
+  { "catppuccin/nvim", name = "catppuccin", priority = 1000, init = function()
+      vim.cmd.colorscheme 'catppuccin'
+  end},
 
   -- Highlight todo, notes, etc in comments
   { 'folke/todo-comments.nvim', event = 'VimEnter', dependencies = { 'nvim-lua/plenary.nvim' }, opts = { signs = false } },
